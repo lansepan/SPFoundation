3882be86fe594cb0f4c222f0312ec8910c74e72c
panmin <lansepan@gmail.com>
2016-03-03 13:03:31 +0800

    commit for version 1.0.4

--- SPFoundationExample/SPFoundation/Macros/SPUtilsMacros.h
+++ SPFoundationExample/SPFoundation/Macros/SPUtilsMacros.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016年 中联苹果实验室. All rights reserved.
 //
 
-#define kRGBColor(r,g,b) [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:1]//RGB颜色转换
+#define SRGBColor(r,g,b) [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:1]//RGB颜色转换
 #define kRGBAColor(r,g,b,a) [UIColor colorWithRed:(r)/255.0f green:(g)/255.0f blue:(b)/255.0f alpha:(a)]//ARGB颜色转换
 #define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
 
