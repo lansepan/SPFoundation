61072567d2d8fe03b026cbeeec8750a9f3342d80
panmin <lansepan@gmail.com>
2016-08-21 17:58:27 +0800

    commit for version 1.6.7

---
+++ SPFoundationExample/SPFoundation/Foundation/NSMutableAttributedString/NSMutableAttributedString+SPExtension.h
@@ -0,0 +1,16 @@
+//
+//  NSMutableAttributedString+SPExtension.h
+//  SPFoundationExample
+//
+//  Created by BluePan on 16/8/21.
+//  Copyright © 2016年 易云世纪科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NSMutableAttributedString (SPExtension)
+
+
+
+@end
